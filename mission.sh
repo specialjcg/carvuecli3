fb002f53e0d1f300b71bc4277069240c9087cae0
Unknown <jc.gouleau@gmail.com>
2019-04-11 23:08:16 +0200

    changement vuecli3

---
+++ mission.sh
@@ -0,0 +1,6 @@
+
+scp -r -p /home/specialjcg/site\ web/carvuecli3/dist/* jcgwebdeveloper@192.168.1.20:/var/www/jcgwebdeveloper/carvuecli3
+
+
+
+
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   configureWebpack: {
     devtool: 'source-map'
-  }
+  },
+
+  publicPath: '/carvuecli3',
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
