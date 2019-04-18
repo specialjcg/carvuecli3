297bc53d38253d3af10e16a4bdeb0032f30cfa77
root <root@Host-004.jcgwebdesigner>
2019-04-18 10:09:45 +0200

    installation d'electron

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ export default class App extends Vue {}
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
  
-  color: #2c3e50;
-  margin-top: 60px;
+  
+
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,102 @@
 <template>
   <div>
-    <div class="parametre">
-    
-    <label for="posy">position en y</label>
-    <b-form-input id="posy" v-model="choixy" type="range" min="-100" max="100" @change="changey" size="1"></b-form-input>
-    <div class="mt-2">Value: {{ choixy }}</div>
-   </div>
-    <div
-      class="maMission"
-      v-for="(task, num) in tasks"
-      :key="num"
-      @mouseenter.prevent="rotationCarroussel()"
-      
-      :id="imginfo(num)"
-    >
-      <div class="maMission2">
-        <div class="transition-maison">
-          <h1>
-            <span>{{ task }}</span>
-          </h1>
+    <section class="styleParametre">
+      <h1 class="titre">Réglage</h1>
+      <div class="parametre">
+        <!--<div class="titre">Réglage</div>-->
+        <button type="button" class="btn btn-3">
+          position en y<span class="badge">{{ choixy }}</span>
+        </button>
+        <br /><br />
+        <b-form-input
+          id="posy"
+          v-model="choixy"
+          type="range"
+          min="-100"
+          max="100"
+          @input="changey"
+          size="1"
+        ></b-form-input>
+      </div>
+      <div class="parametre">
+        <button type="button" class="btn btn-3">
+          position en x<span class="badge">{{ choixx }}</span>
+        </button>
+        <br /><br />
+        <b-form-input
+          id="posx"
+          v-model="choixx"
+          type="range"
+          min="-100"
+          max="100"
+          @input="changex"
+          size="1"
+        ></b-form-input>
+      </div>
+      <div class="parametre">
+        <button type="button" class="btn btn-3">
+          echelle<span class="badge">{{ choixsc }}</span>
+        </button>
+        <br /><br />
+        <b-form-input
+          id="posscale"
+          v-model="choixsc"
+          type="range"
+          min="-100"
+          max="100"
+          @input="changesc"
+          size="1"
+        ></b-form-input>
+      </div>
+      <div class="parametre">
+        <button type="button" class="btn btn-3">
+          largeur panneau<span class="badge">{{ choixlargeur }}</span>
+        </button>
+        <br /><br />
+        <b-form-input
+          id="poslargeur"
+          v-model="choixlargeur"
+          type="range"
+          min="0"
+          max="60"
+          @input="changeWidth"
+          size="1"
+        ></b-form-input>
+      </div>
+      <div class="parametre">
+        <button type="button" class="btn btn-3">
+          hauteur panneau<span class="badge">{{ choixhauteur }}</span>
+        </button>
+        <br /><br />
+        <b-form-input
+          id="poshauteur"
+          v-model="choixhauteur"
+          type="range"
+          min="-100"
+          max="100"
+          @input="choixhauteur"
+          size="1"
+        ></b-form-input>
+      </div>
+    </section>
+    <section class="styleParametre2">
+      <h1 class="titre">Rendu</h1>
+      <div
+        class="maMission"
+        v-for="(task, num) in tasks"
+        :key="num"
+        @mouseenter.prevent="rotationCarroussel()"
+        :id="imginfo(num)"
+      >
+        <div class="maMission2">
+          <div class="transition-maison">
+            <h1>
+              <span>{{ task }}</span>
+            </h1>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
@@ -69,20 +144,46 @@ export default class HelloWorld extends Vue {
     "« J’ai des questions à toutes vos réponses »  Woody Allen -",
     "« Les autres parlent, moi je travaille »picasso"
   ];
+  choixx: number = 0;
+  choixsc: number = 0;
+
+  choixyinter: number = 0;
+  choixxinter: number = 0;
+  choixscinter: number = 0;
+
+  choixhauteur: number = 0;
+  choixlargeur: number = 50;
+  choixlargeurinter: number = 0;
+
   mounted() {
     this.choixy = 0;
+    this.choixx = 0;
+    this.choixsc = 0;
+    this.choixyinter = 0;
+    this.choixxinter = 0;
+    this.choixscinter = 0;
+    this.choixhauteur = 0;
+    this.choixlargeur = 50;
+    this.choixlargeurinter = 0;
     this.index = 0;
-     this.rotationCarroussel()
-  } 
-   imginfo(index1:number) {
-      var essai = "imginfo" + index1;
+    this.rotationCarroussel();
+  }
+  imginfo(index1: number) {
+    var essai = "imginfo" + index1;
 
-      return essai;
-    }
+    return essai;
+  }
   constructor() {
     super();
     this.choixy = 0;
-   
+    this.choixx = 0;
+    this.choixsc = 0;
+    this.choixyinter = 0;
+    this.choixxinter = 0;
+    this.choixscinter = 0;
+    this.choixhauteur = 0;
+    this.choixlargeur = 50;
+    this.choixlargeurinter = 0;
     this.positionCarroussel = new positionCarrousselglobal(
       [0, 0, 0, 0, 0, 0],
       [300, 150, 25, 25, 150, 300],
@@ -92,11 +193,10 @@ export default class HelloWorld extends Vue {
       [1, 0.8, 0.7, 0.6, 0.5, 0.4],
       [6, 5, 4, 3, 2, 1]
     );
-   
   }
-styleclass(num1:number){
-   var tran = "";
-   
+  styleclass(num1: number) {
+    var tran = "";
+
     tran =
       "translate3d(" +
       this.positionCarroussel.x[num1] +
@@ -108,22 +208,28 @@ styleclass(num1:number){
       "px) scale(" +
       this.positionCarroussel.sc[num1] +
       ")";
-    var essai:number = this.positionCarroussel.zindex[num1];
-  var opa = this.positionCarroussel.opa[num1].toString();
-  const myElement = document.getElementById("imginfo" + num1)!;
-  if (myElement!=null){
-     myElement.style.opacity = opa;
-     myElement.style.transition="3s ease";
-     myElement.style.transform=tran;
-     myElement.style.zIndex=essai.toString();
+    var essai: number = this.positionCarroussel.zindex[num1];
+    var opa = this.positionCarroussel.opa[num1].toString();
+    const myElement = document.getElementById("imginfo" + num1)!;
+    if (myElement != null) {
+      myElement.style.opacity = opa;
+      myElement.style.filter = "blur(" + (0.9 - Number(opa)) * 10 + "px)";
 
-     
-     }
-
-
-}
-  
-
+      myElement.style.transition = "3s ease";
+      myElement.style.transform = tran;
+      myElement.style.zIndex = essai.toString();
+    }
+  }
+  changeWidth() {
+    for (var num1 = 0; num1 < 6; num1++) {
+      const myElement = document.getElementById("imginfo" + num1)!;
+      if (myElement != null) {
+        myElement.style.width =
+          this.choixlargeur + "vw";
+      }
+    }
+    this.choixlargeurinter = this.choixlargeur;
+  }
   rotationLignecarroussel(positionCarrousselinter: Array<number>) {
     var interCarroussel = 0;
     interCarroussel = positionCarrousselinter[0];
@@ -136,7 +242,6 @@ styleclass(num1:number){
     return positionCarrousselinter;
   }
   rotationCarroussel() {
-   
     this.positionCarroussel.x = this.rotationLignecarroussel(
       this.positionCarroussel.x
     );
@@ -158,18 +263,41 @@ styleclass(num1:number){
     this.positionCarroussel.zindex = this.rotationLignecarroussel(
       this.positionCarroussel.zindex
     );
-    for (var i = 0; i < this.positionCarroussel.x.length ; i++) {
-       this.styleclass(i)
+    for (var i = 0; i < 6; i++) {
+      this.styleclass(i);
     }
   }
 
   changey(): void {
     for (var i = 0; i < 6; i++) {
-      this.positionCarroussel.y[i] = this.positionCarroussel.y[i] + Number(this.choixy);
+      this.positionCarroussel.y[i] =
+        this.positionCarroussel.y[i] + Number(this.choixy - this.choixyinter);
     }
-    for (i = 0; i < this.positionCarroussel.x.length ; i++) {
-       this.styleclass(i)
+    for (i = 0; i < this.positionCarroussel.x.length; i++) {
+      this.styleclass(i);
     }
+    this.choixyinter = this.choixy;
+  }
+  changex(): void {
+    for (var i = 0; i < 6; i++) {
+      this.positionCarroussel.x[i] =
+        this.positionCarroussel.x[i] + Number(this.choixx - this.choixxinter);
+    }
+    for (i = 0; i < this.positionCarroussel.x.length; i++) {
+      this.styleclass(i);
+    }
+    this.choixxinter = this.choixx;
+  }
+  changesc(): void {
+    for (var i = 0; i < 6; i++) {
+      this.positionCarroussel.sc[i] =
+        this.positionCarroussel.sc[i] +
+        Number(this.choixsc - this.choixscinter) / 500;
+    }
+    for (i = 0; i < this.positionCarroussel.x.length; i++) {
+      this.styleclass(i);
+    }
+    this.choixscinter = this.choixsc;
   }
 }
 </script>
@@ -181,13 +309,28 @@ $color2: rgba(87, 74, 226, 1);
 $color3: rgba(101, 69, 151, 1);
 $color4: rgba(171, 129, 205, 1);
 $color5: rgba(226, 173, 242, 1);
+.btn-3 {
+  background-image: linear-gradient(
+    to right,
+    #84fab0 0%,
+    #8fd3f4 51%,
+    #84fab0 100%
+  );
+}
+.btn:hover {
+  background-position: right center; /* change the direction of the change here */
+}
 .maMission {
   height: 10vw;
   position: absolute;
-  top: 0;
-  left: 20vw;
+	display: inline-block;
+ top: 5vw;
+  left: 10vw;
   bottom: 0;
   right: 0;
+
+
+    
   overflow-x: hidden;
   overflow-y: auto;
   /*perspective: 2px;*/
@@ -197,7 +340,7 @@ $color5: rgba(226, 173, 242, 1);
         bottom: 0;
         left: 50%;*/
   width: 50vw;
-  background-color:$color1;
+  background-color: $color1;
 }
 
 .maMission2 {
@@ -206,7 +349,7 @@ $color5: rgba(226, 173, 242, 1);
   left: 0;
   height: auto;
   width: 50vw;
-  color:$color5;
+  color: $color5;
   text-align: center;
 
   top: 50%;
@@ -214,7 +357,50 @@ $color5: rgba(226, 173, 242, 1);
   transform: translateY(-50%);
   /* tiré de la moitié de sa propre hauteur */
 }
-.parametre{
+
+.styleParametre {
+  position: absolute;
+  top: 1vh;
+  background: $color5;
+  left: 2vw;
+  width: 300px;
+  height: 90vh;
+  border-radius: 1vw;
+  box-shadow: 3px 3px 20px rgba(0, 0, 0, 0.5);
+}
+.styleParametre2 {
+  position: absolute;
+  top: 1vh;
+  background: $color5;
+  left: 25vw;
+  width: 70vw;
+  height: 90vh;
+  border-radius: 1vw;
+  box-shadow: 3px 3px 20px rgba(0, 0, 0, 0.5);
+   text-align: center;
+
+}
+.titre {
+  position: relative;
+  border-top-left-radius: 1vw;
+  border-top-right-radius: 1vw;
+  background-color: $color4;
+  width: auto;
+  letter-spacing: 0vw;
+  font-family: Open Sans;
+  font-size: 1.618em;
+  font-weight: 800;
+  line-height: 1.2;
+  margin: auto;
+  text-align: center;
+  height: auto;
+}
+.parametre,
+button {
+  font-family: Open Sans;
+  color: $color1;
+  margin: auto;
+  margin-top: 1vh;
   max-width: 250px;
 }
 h1 {
@@ -223,6 +409,5 @@ h1 {
   font-size: 1.618em;
   font-weight: 800;
   line-height: 1.2;
-  margin: 1.414em 0 0.5em;
 }
 </style>
