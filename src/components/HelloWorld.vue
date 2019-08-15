36c979a9d8d03a482302577a0cc93ee58fc1a05f
jcgwebdeveloper <jc.gouleau@gmail.com>
2019-08-15 21:45:11 +0200

    modification responsive et hammer

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,123 @@
 <template>
   <div>
-    <section class="styleParametre">
-      <h1 class="titre">Réglage</h1>
-      <div class="parametre">
-        <button class="btn btn-3" @click="messagetext()">
-          changer les citations
-        </button>
+    <div class="container-fluid">
+      <div class="row no-padding">
+        <div class="col-md-3 col-sm-2 styleParametre">
+          <h1 class="titre">Réglage</h1>
+          <div class="parametre col-md-4">
+            <button class="btn btn-3 btn-block btn-responsive " @click="messagetext()">
+              changer les citations
+            </button>
 
-        <!--<div class="titre">Réglage</div>-->
-        <button type="button" class="btn btn-3">
-          position en y
-          <span class="badge">{{ choixy }}</span>
-        </button>
-        <br /><br />
-        <b-form-input
-          id="posy"
-          v-model="choixy"
-          type="range"
-          min="-100"
-          max="100"
-          size="1"
-          @input="changey"
-        ></b-form-input>
-      </div>
-      <div class="parametre">
-        <button type="button" class="btn btn-3">
-          position en x
-          <span class="badge">{{ choixx }}</span>
-        </button>
-        <br /><br />
-        <b-form-input
-          id="posx"
-          v-model="choixx"
-          type="range"
-          min="-100"
-          max="100"
-          size="1"
-          @input="changex"
-        ></b-form-input>
-      </div>
-      <div class="parametre">
-        <button type="button" class="btn btn-3">
-          echelle
-          <span class="badge">{{ choixsc }}</span>
-        </button>
-        <br /><br />
-        <b-form-input
-          id="posscale"
-          v-model="choixsc"
-          type="range"
-          min="-100"
-          max="100"
-          size="1"
-          @input="changesc"
-        ></b-form-input>
-      </div>
-      <div class="parametre">
-        <button type="button" class="btn btn-3">
-          largeur panneau
-          <span class="badge">{{ choixlargeur }}</span>
-        </button>
-        <br /><br />
-        <b-form-input
-          id="poslargeur"
-          v-model="choixlargeur"
-          type="range"
-          min="10"
-          max="60"
-          size="1"
-          @input="changeWidth"
-        ></b-form-input>
-      </div>
-      <div class="parametre">
-        <button type="button" class="btn btn-3">
-          hauteur panneau
-          <span class="badge">{{ choixhauteur }}</span>
-        </button>
-        <br /><br />
-        <b-form-input
-          id="poshauteur"
-          v-model="choixhauteur"
-          type="range"
-          min="1"
-          max="50"
-          size="1"
-          @input="changeHeight"
-        ></b-form-input>
-      </div>
-    </section>
-    <section class="styleParametre2">
-      <h1 class="titre">Rendu</h1>
-      <div
-        v-for="(task, num) in tasks"
-        :id="imginfo(num)"
-        :key="num"
-        class="maMission"
-        @mouseenter.prevent="rotationCarroussel()"
-      >
-        <div class="maMission2">
-          <h1>{{ task.title }}<br />{{ task.author }}</h1>
+            <!--<div class="titre">Réglage</div>-->
+            <button type="button" class="btn btn-3 btn-block btn-responsive ">
+              position en y
+              <span class="badge">{{ choixy }}</span>
+            </button>
+
+            <b-form-input
+              id="posy"
+              v-model="choixy"
+              type="range"
+              min="-100"
+              max="100"
+              size="1"
+              @input="changey"
+            ></b-form-input>
+          </div>
+          <div class="parametre col-md-4">
+            <button type="button" class="btn btn-3 btn-block btn-responsive ">
+              position en x
+              <span class="badge">{{ choixx }}</span>
+            </button>
+
+            <b-form-input
+              id="posx"
+              v-model="choixx"
+              type="range"
+              min="-100"
+              max="100"
+              size="1"
+              @input="changex"
+            ></b-form-input>
+          </div>
+          <div class="parametre col-md-4">
+            <button type="button" class="btn btn-3 btn-block btn-responsive ">
+              echelle
+              <span class="badge">{{ choixsc }}</span>
+            </button>
+
+            <b-form-input
+              id="posscale"
+              v-model="choixsc"
+              type="range"
+              min="-100"
+              max="100"
+              size="1"
+              @input="changesc"
+            ></b-form-input>
+          </div>
+          <div class="parametre col-md-4">
+            <button type="button" class="btn btn-3 btn-block btn-responsive ">
+              largeur panneau
+              <span class="badge">{{ choixlargeur }}</span>
+            </button>
+
+            <b-form-input
+              id="poslargeur"
+              v-model="choixlargeur"
+              type="range"
+              min="10"
+              max="60"
+              size="1"
+              @input="changeWidth"
+            ></b-form-input>
+          </div>
+          <div class="parametre col-md-4">
+            <button type="button" class="btn btn-3 btn-block btn-responsive ">
+              hauteur panneau
+              <span class="badge">{{ choixhauteur }}</span>
+            </button>
+
+            <b-form-input
+              id="poshauteur"
+              v-model="choixhauteur"
+              type="range"
+              min="1"
+              max="50"
+              size="1"
+              @input="changeHeight"
+            ></b-form-input>
+          </div>
+        </div>
+        <div class="col-md-10 col-sm-10 styleParametre2">
+          <h1 class="titre">Rendu</h1>
+          <v-touch
+            :swipe-options="{ threshold: 200 }"
+            @tap="rotationCarroussel()"
+          >
+            <div
+              v-for="(task, num) in tasks"
+              :id="imginfo(num)"
+              :key="num"
+              class="maMission"
+              @mouseenter.prevent="rotationCarroussel()"
+            >
+              <div class="maMission2">
+                <h2>{{ task.title }}<br />{{ task.author }}</h2>
+              </div>
+            </div>
+          </v-touch>
         </div>
       </div>
-    </section>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import io from "socket.io-client";
+
 class citation {
   title: string;
   author: string;
@@ -148,15 +158,33 @@ export default class HelloWorld extends Vue {
   index: number = 0;
   socket: any;
   positionCarroussel: positionCarrousselglobal;
-
+  hauteurEcran: number = 0;
   choixy: number = 0;
   tasks: citation[] = [
-    new citation("Les lois inutiles affaiblissent les lois nécessaires.", "Montesquieu"),
-    new citation(" On a encore à apprendre même après quarante ans de métier.", "Daniel Pennac"),
-    new citation(" La beauté fraîche et vraie ne peut rien cacher.", "George Sand"),
-    new citation("Il y a moins de bonheur à savoir qu'à croire et à espérer.", "Thomas Moore"),
-    new citation("Ce que tu as reçu de tes ancêtres, acquiers-le pour le posséder.", "Goethe"),
-    new citation("Souvent les flatteurs sont exposés à louer des ânes.", " Jean-Louis-Auguste Commerson ;")
+    new citation(
+      "Les lois inutiles affaiblissent les lois nécessaires.",
+      "Montesquieu"
+    ),
+    new citation(
+      " On a encore à apprendre même après quarante ans de métier.",
+      "Daniel Pennac"
+    ),
+    new citation(
+      " La beauté fraîche et vraie ne peut rien cacher.",
+      "George Sand"
+    ),
+    new citation(
+      "Il y a moins de bonheur à savoir qu'à croire et à espérer.",
+      "Thomas Moore"
+    ),
+    new citation(
+      "Ce que tu as reçu de tes ancêtres, acquiers-le pour le posséder.",
+      "Goethe"
+    ),
+    new citation(
+      "Souvent les flatteurs sont exposés à louer des ânes.",
+      " Jean-Louis-Auguste Commerson ;"
+    )
   ];
   choixx: number = 0;
   choixsc: number = 0;
@@ -165,40 +193,61 @@ export default class HelloWorld extends Vue {
   choixxinter: number = 0;
   choixscinter: number = 0;
 
-  choixhauteur: number = 3;
+  choixhauteur: number = 20;
   choixlargeur: number = 50;
   choixlargeurinter: number = 0;
   citationseul: string = "";
 
   mounted() {
-    this.choixy = 0;
+    this.hauteurEcran =
+      0.9 *
+      (window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.body.clientHeight);
+    this.choixy = -20;
     this.choixx = 0;
     this.choixsc = 0;
     this.choixyinter = 0;
     this.choixxinter = 0;
     this.choixscinter = 0;
-    this.choixhauteur = 3;
+    this.choixhauteur = Math.floor(this.hauteurEcran / 15);
     this.choixlargeur = 50;
     this.choixlargeurinter = 0;
     this.index = 0;
     this.tasks = [
-       new citation("Les lois inutiles affaiblissent les lois nécessaires.", "Montesquieu"),
-    new citation(" On a encore à apprendre même après quarante ans de métier.", "Daniel Pennac"),
-    new citation(" La beauté fraîche et vraie ne peut rien cacher.", "George Sand"),
-    new citation("Il y a moins de bonheur à savoir qu'à croire et à espérer.", "Thomas Moore"),
-    new citation("Ce que tu as reçu de tes ancêtres, acquiers-le pour le posséder.", "Goethe"),
-    new citation("Souvent les flatteurs sont exposés à louer des ânes.", " Jean-Louis-Auguste Commerson ;")
+      new citation(
+        "Les lois inutiles affaiblissent les lois nécessaires.",
+        "Montesquieu"
+      ),
+      new citation(
+        " On a encore à apprendre même après quarante ans de métier.",
+        "Daniel Pennac"
+      ),
+      new citation(
+        " La beauté fraîche et vraie ne peut rien cacher.",
+        "George Sand"
+      ),
+      new citation(
+        "Il y a moins de bonheur à savoir qu'à croire et à espérer.",
+        "Thomas Moore"
+      ),
+      new citation(
+        "Ce que tu as reçu de tes ancêtres, acquiers-le pour le posséder.",
+        "Goethe"
+      ),
+      new citation(
+        "Souvent les flatteurs sont exposés à louer des ânes.",
+        " Jean-Louis-Auguste Commerson ;"
+      )
     ];
-var connectionOptions =  {
-              "secure": false,
-            "force new connection" : true,
-            "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
-            "timeout" : 10000, //before connect_error and connect_timeout are emitted.
-            "transports" :["websocket","flashsocket","polling"]
-        };
-   this.socket = io("https://valorisetonweb.fr:3000",connectionOptions);
-
-
+    var connectionOptions = {
+      secure: false,
+      "force new connection": true,
+      reconnectionAttempts: "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
+      timeout: 10000, //before connect_error and connect_timeout are emitted.
+      transports: ["websocket", "flashsocket", "polling"]
+    };
+    this.socket = io("https://valorisetonweb.fr:3000", connectionOptions);
 
     this.rotationCarroussel();
     this.messagetext();
@@ -211,18 +260,31 @@ var connectionOptions =  {
   }
   constructor() {
     super();
-    this.choixy = 0;
+    this.hauteurEcran =
+      0.9 *
+      (window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.body.clientHeight);
+    this.choixy = -20;
     this.choixx = 0;
     this.choixsc = 0;
     this.choixyinter = 0;
     this.choixxinter = 0;
     this.choixscinter = 0;
-    this.choixhauteur = 0;
+    this.choixhauteur = Math.floor(this.hauteurEcran / 15);
     this.choixlargeur = 50;
     this.choixlargeurinter = 0;
+
     this.positionCarroussel = new positionCarrousselglobal(
       [0, 0, 0, 0, 0, 0],
-      [300, 150, 25, 25, 150, 300],
+      [
+        this.hauteurEcran / 15,
+        this.hauteurEcran / 30,
+        this.hauteurEcran / 60,
+        this.hauteurEcran / 60,
+        this.hauteurEcran / 30,
+        this.hauteurEcran / 15
+      ],
       [0, 100, 50, 100, 50, 0],
       [20, 20, 20, 20, 20, 20],
       [0.9, 0.7, 0.7, 0.5, 0.5, 0.3],
@@ -252,7 +314,7 @@ var connectionOptions =  {
       this.positionCarroussel.x[num1] +
       "px, " +
       this.positionCarroussel.y[num1] +
-      "px," +
+      "vh," +
       (this.positionCarroussel.z[num1] +
         num1 * this.positionCarroussel.expand[num1]) +
       "px) scale(" +
@@ -261,14 +323,23 @@ var connectionOptions =  {
     var essai: number = this.positionCarroussel.zindex[num1];
     var opa = this.positionCarroussel.opa[num1].toString();
     const myElement = document.getElementById("imginfo" + num1)!;
+
     if (myElement != null) {
       myElement.style.opacity = opa;
       myElement.style.filter = "blur(" + (0.9 - Number(opa)) * 10 + "px)";
+      let taillePix = Math.sqrt(
+        (myElement.offsetHeight * myElement.offsetWidth) /
+          myElement.innerText.length
+      );
+      for (let i = 1; i < taillePix; i += 0.1) {
+        myElement.style.fontSize = i + "px";
+      }
 
       myElement.style.transition = "3s ease";
       myElement.style.transform = tran;
       myElement.style.zIndex = essai.toString();
     }
+   
   }
   changeWidth() {
     for (var num1 = 0; num1 < 6; num1++) {
@@ -283,6 +354,13 @@ var connectionOptions =  {
       const myElement = document.getElementById("imginfo" + num1)!;
       if (myElement != null) {
         myElement.style.height = this.choixhauteur + "vh";
+        let taillePix = Math.sqrt(
+          (myElement.offsetHeight * myElement.offsetWidth) /
+            myElement.innerText.length
+        );
+        for (let i = 1; i < taillePix; i += 0.1) {
+          myElement.style.fontSize = i + "px";
+        }
       }
     }
   }
@@ -298,6 +376,7 @@ var connectionOptions =  {
     return positionCarrousselinter;
   }
   rotationCarroussel() {
+   
     this.positionCarroussel.x = this.rotationLignecarroussel(
       this.positionCarroussel.x
     );
@@ -377,10 +456,10 @@ $color5: rgba(226, 173, 242, 1);
   background-position: right center; /* change the direction of the change here */
 }
 .maMission {
-  height: 10vw;
+  height: 30vh;
   position: absolute;
 
-  top: 5vw;
+  top: 0vw;
   left: 10vw;
   bottom: 0;
   right: 0;
@@ -390,7 +469,7 @@ $color5: rgba(226, 173, 242, 1);
   /*perspective: 2px;*/
   transform-style: preserve-3d;
   /*top: 50%;
-
+  font-size: calc(1vw + 1vh + 1vmin);
         bottom: 0;
         left: 50%;*/
   width: 50vw;
@@ -451,6 +530,11 @@ $color5: rgba(226, 173, 242, 1);
   text-align: center;
   height: auto;
 }
+.no-padding > [class*="col-"] {
+  padding-right: 0;
+  padding-left: 0;
+}
+
 .parametre,
 button {
   font-family: Open Sans;
@@ -458,12 +542,47 @@ button {
   margin: auto;
   margin-top: 1vh;
   max-width: 250px;
+  font-size: 1.1em;
+  padding:inherit;
 }
 h1 {
   letter-spacing: 0vw;
   font-family: Open Sans;
-  font-size: 1.618em;
+  font-size: 0.8rem; /*1rem = 16px*/
   font-weight: 800;
   line-height: 1.2;
 }
+h2 {
+  letter-spacing: 0vw;
+  font-family: Open Sans;
+  font-size: inherit;
+
+  font-weight: 800;
+  line-height: 1.2;
+}
+@media (max-width: 768px) {
+  .btn-responsive {
+  height: inherit;
+  height: 6.5vh;
+    font-size:65%;
+   
+  }
+   .titre {
+font-size: 0.8rem; /*1rem = 16px*/
+
+
+  }
+}
+
+@media (min-width: 769px) and (max-width: 992px) {
+  .btn-responsive {
+  
+    font-size:90%;
+    
+  }
+ 
+}
+
+
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import BootstrapVue from 'bootstrap-vue'
-
+var VueTouch = require('vue-touch')
 
 
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+Vue.use(VueTouch);
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false;
 
